7fb8a113ea69b34bcf75d8b26f47260a2065cc84
epgokce <ep.gokce@gmail.com>
2023-11-22 19:57:37 +0300

    middleWare connected store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
-import { legacy_createStore as createStore} from "redux";
+import { applyMiddleware, legacy_createStore as createStore} from "redux";
 import reducer from "../reducers/index";
-
+import logger from "redux-logger";
+import { movieWare } from "./middleware/favMovieMiddleware";
 
 const store = createStore(
-    reducer  
+    reducer, applyMiddleware(movieWare, logger)  
     );
 
 export default store;
